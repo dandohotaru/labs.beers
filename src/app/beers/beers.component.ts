7057e74d8cf0ce3acf38230c9f77b6a65b57c59d
Dan Dohotaru <dan.dohotaru@gmail.com>
2017-02-18 16:26:58 +0100

    gix beer search

--- src/app/beers/beers.component.ts
+++ src/app/beers/beers.component.ts
@@ -6,7 +6,8 @@ import { Beer } from './../shared/services/beers.models';
 
 @Component({
   selector: 'app-beers',
-  templateUrl: './beers.component.html'
+  templateUrl: './beers.component.html',
+  styleUrls: ["./beers.component.css"]
 })
 export class BeersComponent implements OnInit {
 
@@ -16,27 +17,30 @@ export class BeersComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private service: BeersService) {
-    route.queryParams.subscribe(p => {
-      this.term = p["q"] || "";
-      console.info(`BeerSearch: ${this.term}`);
-    });
-  }
+    private service: BeersService)
+  { }
 
   ngOnInit() {
 
-    var beers = this.service.load()
-      
-      .subscribe(
-        beers => {
-          if (this.term && this.term.length > 0)
-            this.beers = beers.filter(p => p.name.toLowerCase().includes(this.term.toLowerCase()));
-          else
-            this.beers = beers;
-        },
-        error => {
-          console.error(error);
-        });
+    this.route.queryParams.subscribe(p => {
+      this.term = p["q"] || "";
+      console.info(`BeerSearch: ${this.term}`);
+
+      var beers = this.service.load()
+        .distinctUntilChanged()
+        .subscribe(
+          beers => {
+            if (this.term && this.term.length > 0)
+              this.beers = beers
+                .filter(p => p.name.toLowerCase()
+                .includes(this.term.toLowerCase()));
+            else
+              this.beers = beers;
+          },
+          error => {
+            console.error(error);
+          });
+    });
 
   }
 }
