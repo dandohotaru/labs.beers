1e331d45af62d3c6e8d95c3cbad859a2ec88c4cb
Dan Dohotaru <dan.dohotaru@gmail.com>
2017-03-18 14:35:51 +0100

    work on cards based on colcade

--- src/app/shared/components/cards/cards.component.ts
+++ src/app/shared/components/cards/cards.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
 import { Input, ContentChild, TemplateRef } from '@angular/core';
+import * as Colcade from 'colcade';
 
 @Component({
     selector: 'app-cards',
-    templateUrl: './cards.component.html'
+    templateUrl: './cards.component.html',
+    styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit, OnChanges {
 
@@ -13,11 +15,40 @@ export class CardsComponent implements OnInit, OnChanges {
     @ContentChild(TemplateRef)
     template: TemplateRef<any>;
 
-    constructor() { }
+    @ViewChild('myGrid')
+    mygrid: ElementRef;
+
+    hidden: boolean;
+
+    private colcade = null;
+
+    constructor(private element: ElementRef) { }
 
     ngOnInit(): void {
     }
 
     ngOnChanges(changes: SimpleChanges): void {
+        this.hidden = true;
+        setTimeout(p => {
+            this.arrange();
+            this.hidden = false;
+        }, 0);
+    }
+
+    arrange(): void {
+        if (!this.colcade) {
+            var options = {
+                columns: ".grid-col",
+                items: ".grid-item",
+            };
+
+            //var grid = this.mygrid.nativeElement;
+            var grid = this.element.nativeElement.querySelector('.grid');
+
+            this.colcade = new Colcade(grid, options);
+        }
+        else {
+            this.colcade.reload();
+        }
     }
 }
