9173e33657ca35e86232b00eaefc1b789e521105
Dan Dohotaru <dan.dohotaru@gmail.com>
2017-03-20 12:56:34 +0100

    enhance the look and feel for the beer cards

--- src/app/beers/beer-card.component.ts
+++ src/app/beers/beer-card.component.ts
@@ -6,16 +6,23 @@ import { BeerData } from './../shared/services/beers.models';
   selector: 'beer-card',
   templateUrl: './beer-card.component.html',
   styleUrls: ['./beer-card.component.css'],
-  
+
 })
 export class BeerCardComponent implements OnInit {
 
   @Input()
   beer: BeerData;
 
+  legend: { abv: string, ibu: string, srm: string };
+
   constructor() { }
 
   ngOnInit() {
+    this.legend = {
+      abv: "Alcohol By Volume: how much alcohol is in each brew",
+      ibu: "International Bitterness Units: the bitterness from hops in a beer on a scale of 0 to 100",
+      srm: "Color system used by brewers to specify finished beer and malt color"
+    };
   }
 
 }
