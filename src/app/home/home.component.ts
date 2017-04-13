5e6ed42e358273d524adf31f7ff8228bd619521a
Dan Dohotaru <dan.dohotaru@gmail.com>
2017-04-13 14:32:38 +0300

    implementing modal component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,13 +20,16 @@ export class HomeComponent implements OnInit {
     this.bodyText = 'This text can be updated in modal 1';
   }
 
-
   openModal(id: string) {
     this.modalService.open(id);
   }
 
   closeModal(id: string) {
     this.modalService.close(id);
+  }
+
+  log(message: string){
+    console.log(message);
   }
 
 }
--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,58 +1,99 @@
-﻿import { Component, ElementRef, Input, OnInit, OnDestroy } from '@angular/core';
-import * as $ from 'jquery';
+﻿declare var $: any;
+
+import { Component, ElementRef, OnInit, OnDestroy } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/observable/zip';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/last';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/partition';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/toPromise';
 
 import { ModalService } from './modal.service';
 
 @Component({
     selector: 'modal',
-    templateUrl: './modal.component.html',
-    styleUrls: ['./modal.component.css']
+    templateUrl: './modal.component.html'
 })
-
 export class ModalComponent implements OnInit, OnDestroy {
-    @Input() id: string;
-    private element: any;
 
-    constructor(private modalService: ModalService, private el: ElementRef) {
-        this.element = $(el.nativeElement);
+    @Input() 
+    id: string;
 
+    @Input() 
+    title: string;
+
+    @Output() 
+    opening = new EventEmitter();
+
+    @Output() 
+    opened = new EventEmitter();
+
+    @Output() 
+    closing = new EventEmitter();
+
+    @Output() 
+    closed = new EventEmitter();
+
+    private instance: any;
+
+    constructor(private reference: ElementRef, private modalService: ModalService) {
     }
 
     ngOnInit(): void {
-        let modal = this;
-        if (!this.id) {
-            console.error('modal must have an id');
-            return;
-        }
 
-        //this.element.appendTo('body');
+        this.instance = $(this.reference.nativeElement).find("#myModal");
 
-        this.element.on('click', function (e: any) {
-            var target = $(e.target);
-            if (!target.closest('.modal-body').length) {
-                modal.close();
-            }
-        });
+        Observable.fromEvent(this.instance, "show.bs.modal")
+            .subscribe(p => {
+                this.opening.next();
+            });
+
+        Observable.fromEvent(this.instance, "shown.bs.modal")
+            .subscribe(p => {
+                this.opened.next();
+            });
+
+        Observable.fromEvent(this.instance, "hide.bs.modal")
+            .subscribe(p => {
+                this.closing.next();
+            });
+
+        Observable.fromEvent(this.instance, "hidden.bs.modal")
+            .subscribe(p => {
+                this.closed.next();
+            });
+
 
         this.modalService.add(this);
     }
 
     ngOnDestroy(): void {
         this.modalService.remove(this.id);
-        this.element.remove();
     }
 
     open(): void {
-        //this.element.show();
-        
-        //$('myModal').modal('show');
-        
+        var options = {
+            backdrop: true,
+            keyboard: true,
+            show: true,
+        };
 
-        //$('body').addClass('modal-open');
+        this.instance.modal(options);
     }
 
     close(): void {
-        //this.element.hide();
-        //$('body').removeClass('modal-open');
+        this.instance.modal("hide");
+        //console.log("modal closed")
     }
 }
